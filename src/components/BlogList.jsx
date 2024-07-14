6db83f4b17d6afdaadaea591897f4bf679b85645
viraljain7 <veerljain1234@gmail.com>
2024-07-14 23:29:47 +0530

    feat: Implement filter feature for BlogList

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,20 +1,23 @@
 import { blog_data } from '@/Assets/assets'
-import React from 'react'
+import React, { useState } from 'react'
 import BlogItem from './BlogItem'
 
 const BlogList = () => {
+
+    const [menu, setMenu] = useState("All")
+
     return (
         <div>
 
             <div className='flex justify-center gap-6 my-10'>
-                <button className='bg-black text-white py-1 px-4 rounded-sm'>All</button>
-                <button>Technology</button>
-                <button>Startup</button>
-                <button>Lifestyle</button>
+                <button onClick={() => setMenu("All")} className={menu === "All" ? 'bg-black text-white py-1 px-4 rounded-sm' : ''}>All</button>
+                <button className={menu === "Technology" ? 'bg-black text-white py-1 px-4 rounded-sm' : ''} onClick={() => setMenu("Technology")}>Technology</button>
+                <button className={menu === "Startup" ? 'bg-black text-white py-1 px-4 rounded-sm' : ''} onClick={() => setMenu("Startup")}>Startup</button>
+                <button className={menu === "Lifestyle" ? 'bg-black text-white py-1 px-4 rounded-sm' : ''} onClick={() => setMenu("Lifestyle")}>Lifestyle</button>
             </div>
             <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-                {blog_data.map((item, index) => {
-                    return <BlogItem key={index} image={item.image} title={item.title} description={item.description} />
+                {blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item, index) => {
+                    return <BlogItem key={index} image={item.image} category={item.category} title={item.title} description={item.description} />
                 })}
             </div>
 
