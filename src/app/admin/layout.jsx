67b950eb973094da6b65d9bda812ee520921f9aa
viraljain7 <veerljain1234@gmail.com>
2024-07-17 00:10:01 +0530

    Implement Admin DashBoard

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,21 @@
+import { assets } from "@/Assets/assets";
+import Sidebar from "@/components/AdminComponents/Sidebar";
+import Image from "next/image";
+
+export default function Layout({ children }) {
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-b border-black">
+                        <h3 className="font-medium">Admin Panel</h3>
+                        <Image src={assets.profile_icon} width={40} alt="profile" />
+                    </div>
+                </div>
+            </div>
+
+            {children}
+        </>
+    );
+}
---
+++ src/components/AdminComponents/Sidebar.jsx
@@ -0,0 +1,34 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className='flex flex-col bg-slate-100'>
+            <div className='px-2 sm:pl-14 py-3 border border-black'>
+                <Image src={assets.logo} alt='logo' width={120} />
+            </div>
+            <div className='w-28 sm:w-80 h-[100vh] relative py-12 border border-black'>
+
+                <div className="absolute right-0 w-[50%] sm:w-[80%]">
+                    <Link href={'/admin/addProduct'} className='flex items-center gap-3 font-medium px-3   bg-white shadow-[-5px_5px_0px_#000] py-2 border border-black'>
+                        <Image src={assets.add_icon} alt='blog' width={28} />
+                        Add Blogs
+                    </Link>
+                    <Link href={'/admin/blogList'} className='mt-5 flex items-center gap-3 font-medium px-3 bg-white shadow-[-5px_5px_0px_#000] py-2 border border-black'>
+                        <Image src={assets.blog_icon} alt='blog' width={28} />
+                        Blogs lists
+                    </Link>
+                    <Link href={'/admin/subscriptions'} className='mt-5 flex items-center gap-3 font-medium px-3 bg-white shadow-[-5px_5px_0px_#000] py-2 border border-black'>
+                        <Image src={assets.email_icon} alt='blog' width={28} />
+                        Subscriptions
+                    </Link>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar
