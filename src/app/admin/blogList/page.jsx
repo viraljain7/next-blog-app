946b6f678300ed21bd04c2c9f616d4e4fa636421
viraljain7 <veerljain1234@gmail.com>
2024-07-17 20:56:58 +0530

    feat: Create Delete Blog Feature

---
+++ src/app/admin/blogList/page.jsx
@@ -0,0 +1,48 @@
+"use client"
+import BlogTableItem from '@/components/AdminComponents/BlogTableItem'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const page = () => {
+    const [blogs, setBlogs] = useState([])
+    const fetchBlogs = async () => {
+        const Response = await axios.get("/api/blog")
+        console.log(Response.data);
+
+        setBlogs(Response.data.blogs)
+    }
+
+    useEffect(() => {
+        fetchBlogs()
+    }, [])
+
+
+
+    return (
+        <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <h1>All blogs</h1>
+            <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
+                <table className='w-full text-sm text-gray-500 '>
+
+                    <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
+                        <tr>
+                            <th scope='col' className='hidden sm:block px-6 py-3'>Author Name</th>
+                            <th scope='col' className='px-6 py-3'>Blog Title</th>
+                            <th scope='col' className='px-6 py-3'>Date</th>
+                            <th scope='col' className='px-6 py-3'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {blogs.map((item, index) => {
+                            return <BlogTableItem key={index} title={item.title} author={item.author} authorImg={item.authorImg} mongoId={item._id} date={item.date} />
+                        })}
+                    </tbody>
+
+                </table>
+
+            </div>
+        </div>
+    )
+}
+
+export default page
